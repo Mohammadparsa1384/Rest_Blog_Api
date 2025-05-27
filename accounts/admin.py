ccec2a09b0d236994dacc751a95a8e42e1c8ffa0
parsa <Mohammadparsa1384@users.noreply.github.com>
2025-05-27 21:23:03 +0330

    Custom user model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'is_staff', 'is_active', 'is_superuser', 'is_verified')
+    list_filter = ('is_staff', 'is_active', 'is_superuser', 'is_verified')
+    search_fields = ('email',)
+    ordering = ('email',)
+    
+    fieldsets = (
+        ('Authentication', {
+            "fields": (
+                'email','password',
+            ),
+        }),
+        ('Permissions', {
+            "fields": (
+                'is_staff','is_active','is_superuser','is_verified',
+            ),
+        }),
+        
+        ('group permissions', {
+            "fields": (
+                'groups','user_permissions'
+            ),
+        }),
+        
+        ('Important date', {
+            "fields": (
+                'last_login',
+            ),
+        }),
+    )
+    
+    add_fieldsets = [
+        (
+            None,
+            {
+                "classes": ["wide"],
+                "fields": ["email", "password1", "password2","is_staff", "is_active", 'is_superuser','is_verified'],
+            },
+        ),
+    ]
+
+admin.site.register(CustomUser, CustomUserAdmin)
