e674d6010ae1a33b7f4732e6a0abcc92e75435cd
parsa <Mohammadparsa1384@users.noreply.github.com>
2025-05-31 14:30:02 +0330

    ]mplement password reset with JWT token and expiry check

--- accounts/api/v1/urls.py
+++ accounts/api/v1/urls.py
@@ -16,6 +16,11 @@ urlpatterns = [
     # change password
     path("change-password/", views.PasswordChangeApiView.as_view() , name="change-password"),
     
+    # password reset
+    path("password-reset/",views.PasswordResetRequestApiView.as_view(), name="password-reset-request"),
+    path('password/reset/confirm/<str:token>', views.PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
+    
+    
     
     # login jwt
     path("jwt/create/", views.CustomTokenObtainPairView.as_view(), name="jwt-create"),
