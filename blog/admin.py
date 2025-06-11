00b9740d43e676b678688d69f3083dfe3a206565
parsa <Mohammadparsa1384@users.noreply.github.com>
2025-06-11 18:21:46 +0330

    Implement blog models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Post, Category, Tag
 
-# Register your models here.
+# Register yourm models here
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'status', 'created_date')
+    list_filter = ('status', 'created_date', 'category', 'tags')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    autocomplete_fields = ['author','category', 'tags']
+    date_hierarchy = 'created_date'
+    ordering = ('-created_date',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug')
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ('title',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
+
--- config/urls.py
+++ config/urls.py
@@ -28,5 +28,5 @@ urlpatterns = [
     path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
         
-    path("accounts/", include('accounts.urls'))
+    path("accounts/", include('accounts.urls')),
 ]
